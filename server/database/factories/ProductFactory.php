324eb7dc97d9e2711c838f4c4fbc58fbd8ef56bd
Nehuen Cuenca <nehuen.cuenca@hotmail.com>
2023-11-24 16:30:42 -0300

    Configurated env file, and generated model class for Product table (migration, factory, seeder, controller)

---
+++ server/database/factories/ProductFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Product>
+ */
+class ProductFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => fake()->words($nb = 3, $asText = true),
+            'description' => fake()->text($maxNbChars = 150),
+            'price_cost' => fake()->randomFloat($nbMaxDecimals = 2, $min = 0, $max = 8)
+        ];
+    }
+}
