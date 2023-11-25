9ccaeaa4ac87d4ded963a131097e21ebebd8595b
Nehuen Cuenca <nehuen.cuenca@hotmail.com>
2023-11-25 00:25:16 -0300

    Created model classes for Role (msfc), added the relationship ManyToMany with User model, created RoleUser migration and seeder

--- server/database/seeders/DatabaseSeeder.php
+++ server/database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Database\Eloquent\Factories\Factory;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,6 +16,14 @@ class DatabaseSeeder extends Seeder
         \App\Models\Brand::factory(10)->create();
         \App\Models\Category::factory(10)->create();
         \App\Models\Product::factory(20)->create();
+        $users = \App\Models\User::factory(8)->create();
+        $roles = \App\Models\Role::factory(3)->create();
+
+        // Associate roles with users
+        $users->each(function ($user) use ($roles) {
+            // $user->roles()->attach($roles->random(), ['additional_column' => 'value']);
+            $user->roles()->attach($roles->random());
+        });
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
