d089d7d4a334cb213c7ab54e8076c46a2582b4c5
Nehuen Cuenca <nehuen.cuenca@hotmail.com>
2023-12-15 20:47:21 -0300

    created page product/name

--- client/components/GridProductItem.vue
+++ client/components/GridProductItem.vue
@@ -1,8 +1,8 @@
 <template>
     <li class="w-60 bg-gray-300 rounded-lg flex flex-col px-3 py-5 gap-y-1">
-        <img src="https://picsum.photos/id/299/240/150" alt="">
+        <img src="https://picsum.photos/id/133/240/150" alt="">
         <span class="font-semibold">$99.99</span>
-        <NuxtLink class="font-semibold" to="#">Product name..</NuxtLink>
+        <NuxtLink class="font-semibold" to="/product/random">Product name..</NuxtLink>
     </li>
 </template>
 
---
+++ client/pages/product/[name].vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="py-10 px-16 grid grid-cols-2 grid-rows-[repeat(4,minmax(0,_auto)] gap-4">
+        <img class="row-span-3 col-span-1 h-80 rounded-sm object-cover shadow-xl" src="https://picsum.photos/id/133/1024/350" alt="picsum photo">
+        <h1 class="col-start-2 font-semibold text-3xl">Nombre de producto</h1>
+        <h3 class="col-start-2 font-semibold text-3xl">$99.99</h3>
+        <div class="col-start-2 w-2/6 rounded-xl shadow-md h-fit p-3 flex justify-around items-center bg-beigeStrong">
+            <button class="rounded-sm bg-gray-300 px-2 text-lg font-medium">-</button>
+            <span class="text-xl font-bold">0</span>
+            <button class="rounded-sm bg-gray-300 px-2 text-lg font-medium">+</button>
+        </div>
+        <div>
+            <h3 class="mb-3 underline font-medium text-gray-600 text-xl">Description</h3>
+            <p class="font-medium">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellat saepe sint recusandae. Optio blanditiis ullam ea cum, et dignissimos aperiam sapiente autem obcaecati voluptatum eligendi, aliquam unde velit repellendus eveniet!</p>
+        </div>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
