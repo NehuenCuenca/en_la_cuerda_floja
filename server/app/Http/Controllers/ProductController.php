c0baa24990f18c3173a1d110e5c2770690566716
Nehuen Cuenca <nehuen.cuenca@hotmail.com>
2023-12-29 16:43:50 -0300

    added routes for the ProductController to get products (all, by brand, category, or both)

--- server/app/Http/Controllers/ProductController.php
+++ server/app/Http/Controllers/ProductController.php
@@ -2,9 +2,89 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Brand;
+use App\Models\Category;
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    //
+    public function getAllProducts(Request $request)
+    {
+        $paginateBy = intval($request->input('paginateBy')) ?: 10;
+        $paginated  = Product::paginate($paginateBy);
+        return response()->json($paginated);
+    }
+
+    public function getProductsByCategory(Request $request, $category)
+    {
+        $productsByCategory = Category::where('name', $category)->first()->products->all();
+        dd($productsByCategory);
+        return response()->json([
+            'products' => $productsByCategory
+        ]);
+    }
+
+    public function getProductsByBrand(Request $request, $brand)
+    {
+        $productsByBrand = Brand::where('name', $brand)->first()->products->all();
+        dd($productsByBrand);
+        return response()->json([
+            'products' => $productsByBrand
+        ]);
+    }
+
+    public function getProductsByFilters(Request $request)
+    {
+        $filtersFromUrl = $request->collect()->all();
+
+        // CHECK IF THERE ARE FILTERS ON THE URL
+        if (empty($filtersFromUrl)) {
+            return response()->json([
+                'message' => "Send at least one filter (brand or category) on the query params. Returned all products",
+                "products" => Product::all()
+            ]);
+        }
+
+        $brandFounded = null;
+        $categoryFounded = null;
+
+        // CHEACK IF THE FILTERS EXISTS AND ARE VALIDS
+        if (isset($filtersFromUrl['brand'])) {
+            $brandFounded = Brand::where('name', $filtersFromUrl['brand'])->first();
+        }
+        if (isset($filtersFromUrl['category'])) {
+            $categoryFounded = Category::where('name', $filtersFromUrl['category'])->first();
+        }
+
+        // Clean the null filters (when the category or brand no exist)
+        $sanitizedFilters = array_filter(
+            // Map of the valid filters E.g: [ ['brand_id', '=', '1'] ]
+            array_map(
+                function ($recordFounded, $filterFromUrl) {
+                    if (!isset($recordFounded)) {
+                        return;
+                    }
+                    return array_values(["$filterFromUrl" . "_id", "=", "$recordFounded->id"]);
+                },
+                array_filter([$brandFounded, $categoryFounded]),
+                array_keys($filtersFromUrl)
+            )
+        );
+
+        // If the filters after the sanitize are empty...
+        if( empty($sanitizedFilters) ){
+            return response()->json([
+                'message' => "Filters specified may not exist or can't be applied. Returned all products",
+                'products' => Product::all(),
+            ]);
+        }
+
+        $filteredProducts = Product::where($sanitizedFilters)->get();
+
+        return response()->json([
+            'message' => "Filtered products",
+            'products' => $filteredProducts,
+        ]);
+    }
 }
--- server/routes/api.php
+++ server/routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\UserAuthController;
+use App\Http\Controllers\ProductController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,6 +20,7 @@ use App\Http\Controllers\UserAuthController;
 //     return $request->user();
 // });
 
+// USER AUTH
 Route::post('/register', [UserAuthController::class, 'register_user'])->name('api-register');
 Route::post('/login', [UserAuthController::class, 'login_user'])->name('api-login');
 
@@ -26,3 +28,9 @@ Route::middleware(['auth:sanctum'])->group(function () {
     Route::get('/logout', [UserAuthController::class, 'logout_user'])->name('api-logout');
     Route::get('/verify-token', [UserAuthController::class, 'verify_token'])->name('api-verify-token');
 });
+
+// PRODUCTS
+Route::get('/products', [ProductController::class, 'getAllProducts']);
+Route::get('/products/category/{category}', [ProductController::class, 'getProductsByCategory']);
+Route::get('/products/brand/{brand}', [ProductController::class, 'getProductsByBrand']);
+Route::get('/products/filter', [ProductController::class, 'getProductsByFilters']);
