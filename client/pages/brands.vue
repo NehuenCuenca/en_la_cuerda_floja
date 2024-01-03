26b32e0bdd0c953a111e2ffb2de6fbe88a7ba1cf
Nehuen Cuenca <nehuen.cuenca@hotmail.com>
2024-01-03 17:04:19 -0300

    fetched the brands and categories from the api on respectives pages

--- client/pages/brands.vue
+++ client/pages/brands.vue
@@ -1,8 +1,9 @@
 <template>
     <div class="p-9 flex flex-col gap-y-8 justify-start items-center">
         <h3 class="text-4xl font-semibold">Marcas</h3>
-        <ul class="flex justify-around items-start gap-8 flex-wrap">
-            <li v-for="(brand, i) in 35" :key="i" class=" p-2.5 border-none rounded-tr-lg rounded-bl-lg text-brown-200 text-lg font-semibold bg-brown-900 hover:bg-brown-400 hover:text-brown-900 cursor-pointer transition-colors">Marca {{ i }}</li>
+        <h1 v-if="loadingBrands" class="text-xl font-normal">Cargando... ⌛</h1>
+        <ul v-else class="flex justify-around items-start gap-8 flex-wrap">
+            <li v-for="({name}, i) in brands" :key="i" class="p-2.5 border-none rounded-tr-lg rounded-bl-lg text-brown-200 text-lg font-semibold bg-brown-900 hover:bg-brown-400 hover:text-brown-900 cursor-pointer transition-colors">{{ name }}</li>
         </ul>
     </div>
 </template>
@@ -11,6 +12,35 @@
 useHead({
   title: 'Marcas | En la cuerda floja',
 })
+
+// STATE
+const brands = ref([])
+const loadingBrands = ref(true)
+
+
+// LIFECYCLE HOOKS
+onMounted( async() => {
+    brands.value = await getBrands()
+})
+
+// METHODS
+ const getBrands = async() => { 
+    const urlAPIBrands = `http://127.0.0.1:8000/api/brands`
+    const { data, pending, error } = await useFetch(urlAPIBrands)
+    loadingBrands.value = pending.value
+
+    if( error.value ){
+        console.log('Error al traer las marcas: ', error.value.message);
+        return []
+    }
+
+    if( !data.value ){
+        return getBrands(); 
+    }
+    
+    return data.value.data
+}
+
 </script>
 
 <style scoped>
--- client/pages/categories.vue
+++ client/pages/categories.vue
@@ -1,8 +1,9 @@
 <template>
-    <div class="bg-beigeStrong py-40 flex flex-col gap-y-8 justify-start items-center">
+    <div class="bg-beigeStrong min-h-[70vh] p-9 flex flex-col gap-y-8 justify-start items-center">
         <h3 class="text-4xl font-semibold">Categorias</h3>
-        <ul class="flex justify-around items-start gap-8 flex-wrap">
-            <li v-for="(category, i) in 4" :key="i" class=" p-2.5 border-none rounded-tr-lg rounded-bl-lg text-brown-200 text-lg font-semibold bg-brown-900 hover:bg-brown-400 hover:text-brown-900 cursor-pointer transition-colors">Categoria {{ i }}</li>
+        <h1 v-if="loadingCategories" class="text-xl font-normal">Cargando... ⌛</h1>
+        <ul v-else class="flex justify-around items-start gap-8 flex-wrap">
+            <li v-for="({name}, i) in categories" :key="i" class=" p-2.5 border-none rounded-tr-lg rounded-bl-lg text-brown-200 text-lg font-semibold bg-brown-900 hover:bg-brown-400 hover:text-brown-900 cursor-pointer transition-colors">{{ name }}</li>
         </ul>
     </div>
 </template>
@@ -11,6 +12,33 @@
 useHead({
   title: 'Categorias | En la cuerda floja',
 })
+
+// STATE
+const categories = ref([])
+const loadingCategories = ref(true)
+
+// LIFECYCLE HOOKS
+onMounted( async() => {
+    categories.value = await getCategories()
+})
+
+// METHODS
+ const getCategories = async() => { 
+    const urlAPICategories = `http://127.0.0.1:8000/api/categories`
+    const { data, pending, error } = await useFetch(urlAPICategories)
+    loadingCategories.value = pending.value
+
+    if( error.value ){
+        console.log('Error al traer las categorias: ', error.value.message);
+        return []
+    }
+
+    if( !data.value ){
+        return getCategories() ; 
+    }
+    
+    return data.value.data
+}
 </script>
 
 <style scoped>
